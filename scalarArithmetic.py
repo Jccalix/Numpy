158a444a346bc1d1c1e1aba7287f3bb6f5f29636
Jccalix <jccalix6@gmail.com>
2025-09-26 19:20:03 -0600

    Add examples for scalar arithmetic and vectorized math functions using numpy

---
+++ scalarArithmetic.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+array = np.array([1, 2, 3])
+
+print(array + 5)  # Adding a scalar to each element
+print(array - 2)  # Subtracting a scalar from each element
+print(array * 3)  # Multiplying each element by a scalar
+print(array / 2)  # Dividing each element by a scalar
+print(array ** 2) # Squaring each element
+print(array % 2)  # Modulus operation on each element
+print('--------------------------------')
---
+++ vectorizedMathFunctions.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+array = np.array([1, 2, 3])
+
+print(np.sqrt(array))  # Square root of each element
+print(np.round(array))   # Round each element
+print(np.pi * array)     # Pi times each element
